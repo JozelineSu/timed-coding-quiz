c47e2029b5cc24f85230c58483fe39260dd20b3d
unknown <jozelinesuarez@gmail.com>
2023-08-30 21:39:58 -0500

    Condensed css and started getting references to html in js sheet

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// get variables to connect to html
+var startBtn = document.querySelector(".start-quizbtn");
+var initialsBtn = document.querySelector(".initials-submitbtn");
+var goBackBtn = document.querySelector(".go-back");
+var clearScoresBtn = document.querySelector(".clear-scores");
+
+//If Start Quiz Button Clicked
+startBtn.onclick = function() {
+    
+}
